9e563766effa906979f79c7c3d8f47046a34d77a
AlexisLara19 <alexis191195@hotmail.com>
2024-03-18 22:41:20 -0600

    third commit

--- main.py
+++ main.py
@@ -9,9 +9,12 @@ def print_hi(name):
     print(f'Hi popochen , {name}')  # Press Ctrl+F8 to toggle the breakpoint.
     print('Finaliza la funcion')
 
+    print('Aqui inicia otra funcion')
+
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
+    print('Estoy afuera de la funcion')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
